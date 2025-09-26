bb435aded7e93d29d5a6178d802b2436d8d61f01
MikeHi1 <mikeneverd3@gmail.com>
2025-09-26 13:46:29 -0700

    (GP-2.1.1): Repository Initialization Testing
    Implements a test method to verify the correct creation of a Git repository, including the 'git' directory, 'objects' subdirectory, and 'index' and 'HEAD' files.
    
    This test is run multiple times and includes deletion of created
    files after each run to ensure consistent test execution.

--- Git.java
+++ Git.java
@@ -48,16 +48,63 @@ public class Git{
         return file;
     }
 
+    public static boolean test() throws IOException{
+        for (int i = 0; i < 3; i++){
+            File git = makeFolder("git");
+            File objects = makeFolder("objects", git);
+            File index = makeFile("index", git);
+            File HEAD = makeFile("HEAD", git);
+            if (AlreadyExists){
+                System.out.println("Git Repository Already Exists");
+            } else{
+                System.out.println("Git Repository Created");
+            }
+
+            File gitTest = new File("git");
+            if (!gitTest.exists()){
+                System.out.println("failed! " + gitTest.getName() + " doesn't exist!");
+                return false;
+            }
+
+            File objectsTest = new File(git.getPath(),"objects");
+            if (!objectsTest.exists()){
+                System.out.println("failed! " + objectsTest.getName() + " doesn't exist!");
+                return false;
+            }
+
+            File HEADTest = new File(git.getPath(),"HEAD");
+            if (!HEADTest.exists()){
+                System.out.println("failed! " + HEADTest.getName() + " doesn't exist!");
+                return false;
+            }
+
+            File indexTest = new File(git.getPath(),"index");
+            if (!indexTest.exists()){
+                System.out.println("failed! " + indexTest.getName() + " doesn't exist!");
+                return false;
+            }
+            objects.delete();
+            HEAD.delete();
+            index.delete();
+            git.delete();
+        }
+        return true;
+
+    }
+
     public static void main(String[] args) throws IOException {
         File git = makeFolder("git");
         File objects = makeFolder("objects", git);
         File index = makeFile("index", git);
         File HEAD = makeFile("HEAD", git);
+        
         if (AlreadyExists){
             System.out.println("Git Repository Already Exists");
         } else{
             System.out.println("Git Repository Created");
         }
+
+        System.out.println(test());
         
     }
 }
